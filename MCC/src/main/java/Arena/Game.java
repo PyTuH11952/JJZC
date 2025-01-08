2dd61a4b7149a94b1efc5f08b87fb1bf40724872
PyTuH1952 <andrey15042007@gmail.com>
2025-01-08 18:04:21 +0300

    add getkit

--- MCC/src/main/java/Arena/Game.java
+++ MCC/src/main/java/Arena/Game.java
@@ -2,6 +2,7 @@ package Arena;
 
 import Utils.ChatUtil;
 import com.mimikcraft.mcc.ExecutableApi;
+import com.ssomar.score.usedapi.VaultAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
@@ -24,7 +25,59 @@ public class Game {
 
     private void getkit(Player player){
 
-        ExecutableApi.giveExecutableItem(player, "Prem_Tornado_Blade", 1);
+        if (player.hasPermission("default")) {
+
+            ExecutableApi.giveExecutableItem(player, "case1", 3);
+
+        } else if (player.hasPermission("vip")) {
+            ExecutableApi.giveExecutableItem(player, "case1", 10);
+            ExecutableApi.giveExecutableItem(player, "bomba1", 3);
+            ExecutableApi.giveExecutableItem(player, "bomba2", 2);
+        } else if (player.hasPermission("vip+")) {
+            ExecutableApi.giveExecutableItem(player, "case1", 10);
+            ExecutableApi.giveExecutableItem(player, "case2", 3);
+            ExecutableApi.giveExecutableItem(player, "bomba1", 5);
+            ExecutableApi.giveExecutableItem(player, "bomba2", 3);
+
+        } else if (player.hasPermission("premium")) {
+            ExecutableApi.giveExecutableItem(player, "case1", 15);
+            ExecutableApi.giveExecutableItem(player, "case2", 5);
+            ExecutableApi.giveExecutableItem(player, "case3", 3);
+            ExecutableApi.giveExecutableItem(player, "bomba1", 7);
+            ExecutableApi.giveExecutableItem(player, "bomba2", 5);
+            ExecutableApi.giveExecutableItem(player, "bomba3", 3);
+
+        } else if (player.hasPermission("sponsor")) {
+            ExecutableApi.giveExecutableItem(player, "case1", 16);
+            ExecutableApi.giveExecutableItem(player, "case2", 6);
+            ExecutableApi.giveExecutableItem(player, "case3", 4);
+            ExecutableApi.giveExecutableItem(player, "bomba1", 8);
+            ExecutableApi.giveExecutableItem(player, "bomba2", 6);
+            ExecutableApi.giveExecutableItem(player, "bomba3", 4);
+
+        } else if (player.hasPermission("elite")) {
+            ExecutableApi.giveExecutableItem(player, "case1", 16);
+            ExecutableApi.giveExecutableItem(player, "case2", 5);
+            ExecutableApi.giveExecutableItem(player, "case3", 3);
+            ExecutableApi.giveExecutableItem(player, "case4", 2);
+            ExecutableApi.giveExecutableItem(player, "bomba1", 8);
+            ExecutableApi.giveExecutableItem(player, "bomba2", 6);
+            ExecutableApi.giveExecutableItem(player, "bomba3", 4);
+            ExecutableApi.giveExecutableItem(player, "lom", 1);
+
+        } else if (player.hasPermission("god")) {
+            ExecutableApi.giveExecutableItem(player, "case1", 16);
+            ExecutableApi.giveExecutableItem(player, "case2", 5);
+            ExecutableApi.giveExecutableItem(player, "case3", 3);
+            ExecutableApi.giveExecutableItem(player, "case4", 2);
+            ExecutableApi.giveExecutableItem(player, "bomba1", 8);
+            ExecutableApi.giveExecutableItem(player, "bomba2", 6);
+            ExecutableApi.giveExecutableItem(player, "bomba3", 4);
+            ExecutableApi.giveExecutableItem(player, "bomba5", 2);
+            ExecutableApi.giveExecutableItem(player, "lom2", 1);
+
+        }
+
 
     }
     private void preparePlayers(){
