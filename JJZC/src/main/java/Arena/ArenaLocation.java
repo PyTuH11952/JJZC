ec67cc5904fc83ae368df496d731ad7cca21fd43
PahomkA <101577783+PahomkA@users.noreply.github.com>
2025-01-21 23:18:53 +0300

    Small update

--- JJZC/src/main/java/Arena/ArenaLocation.java
+++ JJZC/src/main/java/Arena/ArenaLocation.java
@@ -1,9 +1,27 @@
 package Arena;
 
+import com.mimikcraft.mcc.Main;
+import io.lumine.mythic.bukkit.utils.config.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ArenaLocation {
 
+    public ArenaLocation(){
+        File folder = new File(Main.getInstance().getDataFolder().getAbsolutePath());
 
+        File file = new File(folder.getAbsolutePath() + "/Locations.yml");
 
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+    }
 
     public enum LocationTypes {
         HOSPITAL,
