2566c663d868326d8fc7f2de3e5299caf18b2ad1
PahomkA <101577783+PahomkA@users.noreply.github.com>
2025-01-24 21:41:19 +0300

    Small update
    Changed ArenaLocation class and Technical task

--- JJZC/src/main/java/Arena/ArenaLocation.java
+++ JJZC/src/main/java/Arena/ArenaLocation.java
@@ -1,14 +1,27 @@
 package Arena;
 
 import com.mimikcraft.mcc.Main;
+import io.lumine.mythic.bukkit.utils.config.ConfigurationSection;
 import io.lumine.mythic.bukkit.utils.config.file.YamlConfiguration;
+import org.bukkit.Location;
+import org.bukkit.World;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class ArenaLocation {
 
-    public ArenaLocation(){
+    public LocationTypes locationType;
+
+    private final Location spawnPosition;
+    private final List<Location> chests = new ArrayList<>();
+    private final List<Stage> stages = new ArrayList<>();
+
+    public ArenaLocation(LocationTypes locationType, World world){
         File folder = new File(Main.getInstance().getDataFolder().getAbsolutePath());
 
         File file = new File(folder.getAbsolutePath() + "/Locations.yml");
@@ -21,6 +34,25 @@ public class ArenaLocation {
             }
         }
         YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+        ConfigurationSection locationSection = config.getConfigurationSection(locationType.toString().toLowerCase());
+        String[] coordinatesStr = locationSection.getString("spawnLocation").split(" ");
+        double spawnX = Double.parseDouble(coordinatesStr[0]);
+        double spawnY = Double.parseDouble(coordinatesStr[1]);
+        double spawnZ = Double.parseDouble(coordinatesStr[2]);
+        spawnPosition = new Location(world, spawnX, spawnY, spawnZ);
+        List<String> chestsCoordinatesStr = locationSection.getStringList("chests");
+        for(String chestCord : chestsCoordinatesStr){
+            String[] chestCordsStr = chestCord.split(" ");
+            double x = Double.parseDouble(chestCordsStr[0]);
+            double y = Double.parseDouble(chestCordsStr[1]);
+            double z = Double.parseDouble(chestCordsStr[2]);
+            chests.add(new Location(world, x, y, z));
+        }
+        List<Object> = locationSection.getList
+    }
+
+    public Location getSpawnPosition() {
+        return spawnPosition;
     }
 
     public enum LocationTypes {
@@ -34,8 +66,11 @@ public class ArenaLocation {
 
 
 class Stage{
-    public Stage(int num){
-
+    Location[] spawners;
+    int wavesCount;
+    public Stage(Location[] spawners, int wavesCount){
+        this.spawners = spawners;
+        this.wavesCount = wavesCount;
     }
 }
 
