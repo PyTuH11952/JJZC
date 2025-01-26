99d0a6ea783fc3fd2e304a5004ca044dad5b181d
PahomkA <101577783+PahomkA@users.noreply.github.com>
2025-01-26 13:08:00 +0300

    Finished ArenaLocation class

--- JJZC/src/main/java/Arena/ArenaLocation.java
+++ JJZC/src/main/java/Arena/ArenaLocation.java
@@ -14,14 +14,21 @@ import java.util.*;
 
 public class ArenaLocation {
 
-    public LocationTypes locationType;
+    private LocationTypes locationType;
 
     private Location spawnPosition;
     private final List<Location> chests = new ArrayList<>();
     private final Map<String, Double> zombies = new HashMap<>();
     private final List<Stage> stages = new ArrayList<>();
+    private World world;
 
     public ArenaLocation(LocationTypes locationType, World world){
+        this.world = world;
+        setLocationType(locationType);
+    }
+
+    public void setLocationType(LocationTypes locationType) {
+        this.locationType = locationType;
         File folder = new File(Main.getInstance().getDataFolder().getAbsolutePath());
 
         File file = new File(folder.getAbsolutePath() + "/Locations.yml");
@@ -41,16 +48,32 @@ public class ArenaLocation {
         double spawnZ = Double.parseDouble(coordinatesStr[2]);
         spawnPosition = new Location(world, spawnX, spawnY, spawnZ);
         List<String> chestsCoordinatesStr = locationSection.getStringList("chests");
-        for(String chestCord : chestsCoordinatesStr){
-            String[] chestCordsStr = chestCord.split(" ");
+        for(String chestCords : chestsCoordinatesStr){
+            String[] chestCordsStr = chestCords.split(" ");
             double x = Double.parseDouble(chestCordsStr[0]);
             double y = Double.parseDouble(chestCordsStr[1]);
             double z = Double.parseDouble(chestCordsStr[2]);
             chests.add(new Location(world, x, y, z));
         }
 
-        for(Map.Entry<String, Object> entry: locationSection.getValues(false).entrySet()){
+        for(Map.Entry<String, Object> entry: locationSection.getConfigurationSection("zombies").getValues(false).entrySet()){
             zombies.put(entry.getKey(), Double.parseDouble(entry.getValue().toString()));
+        }
+
+        ConfigurationSection stagesSection = locationSection.getConfigurationSection("stages");
+        Set<String> keys = stagesSection.getKeys(false);
+        for(String section : keys){
+            List<String> spawnersCoordinatesStr = locationSection.getStringList("stages." + section + ".spawners");
+            List<Location> tempCordsList = new ArrayList<>();
+            for(String spawnerCords : spawnersCoordinatesStr){
+                String[] spawnerCordsStr = spawnerCords.split(" ");
+                double x = Double.parseDouble(spawnerCordsStr[0]);
+                double y = Double.parseDouble(spawnerCordsStr[1]);
+                double z = Double.parseDouble(spawnerCordsStr[2]);
+                tempCordsList.add(new Location(world, x, y, z));
+            }
+            int wavesCount = stagesSection.getInt(section + ".wavesCount");
+            stages.add(new Stage(tempCordsList, wavesCount));
         }
     }
 
@@ -81,9 +104,9 @@ public class ArenaLocation {
 
 
 class Stage{
-    Location[] spawners;
+    List<Location> spawners;
     int wavesCount;
-    public Stage(Location[] spawners, int wavesCount){
+    public Stage(List<Location> spawners, int wavesCount){
         this.spawners = spawners;
         this.wavesCount = wavesCount;
     }
